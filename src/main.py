4cdd5ddd34406be0c4c313c2cec2c63ca7c042be
YuIzawa <yuizalp@gmail.com>
2016-10-30 03:53:40 +0900

    change directory structure

--- src/main.py
+++ src/main.py
@@ -12,43 +12,43 @@ def ba10000_show():
     BA10000.txtを読み込んでグラフを表示する
     :return:
     """
-    G = nx.read_edgelist("data/input/BA10000.txt", nodetype=int)
+    G = nx.read_edgelist("../data/input/BA10000.txt", nodetype=int)
     pos = {}
     nx.draw(G, pos)
-    plt.savefig("data/output/test_graph.png")
+    plt.savefig("../data/output/test_graph.png")
     plt.show()
 
 
 def twitter_sampling():
-    G = nx.read_edgelist("data/input/twitter_combined.txt", nodetype=int)
+    G = nx.read_edgelist("../data/input/twitter_combined.txt", nodetype=int)
     print(gs.cluster_coefficient_average(G))
     print(gs.random_walk_sampling_cca(graph=G, size=100000, metropolized=True))
     print(gs.random_walk_aggregation(graph=G, size=10000, metropolized=True))
 
 
 def youtube_sampling():
-    G = nx.read_edgelist("data/input/com-youtube.ungraph.txt", nodetype=int)
+    G = nx.read_edgelist("../data/input/com-youtube.ungraph.txt", nodetype=int)
     print(gs.random_walk_sampling_cca(graph=G, size=10000))
     print(gs.random_walk_aggregation(graph=G, size=10000, metropolized=False))
 
 
 def youtube_sampling_show(size):
-    G = nx.read_edgelist("data/input/com-youtube.ungraph.txt", nodetype=int)
+    G = nx.read_edgelist("../data/input/com-youtube.ungraph.txt", nodetype=int)
     nodes = list(gs.random_walk(graph=G, size=size))
     graph = nx.Graph()
     graph.add_path(nodes)
     nx.draw_random(G=graph)
-    plt.savefig("data/output/com-youtube.ungraph.rw." + str(size) + ".png")
+    plt.savefig("../data/output/com-youtube.ungraph.rw." + str(size) + ".png")
     plt.show()
 
 
 def amazon_sampling():
-    G = nx.read_edgelist("data/input/com-amazon.ungraph.txt")
+    G = nx.read_edgelist("../data/input/com-amazon.ungraph.txt")
     graph = nx.Graph()
     nodes = gs.random_walk(graph=G, size=10000)
     graph.add_path(nodes)
     json_graph.node_link_data(graph)
-    plt.savefig("data/output/com-amazon.ungraph.rw.png")
+    plt.savefig("../data/output/com-amazon.ungraph.rw.png")
     plt.show()
 
 
