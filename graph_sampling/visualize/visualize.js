d008bbf1789760ee4a568fc5d4f8278cd15e1b05
YuIzawa <yuizalp@gmail.com>
2016-11-07 18:21:48 +0900

    cutout method

--- graph_sampling/visualize/visualize.js
+++ graph_sampling/visualize/visualize.js
@@ -1,58 +1,92 @@
-var w = 800,
-    h = 600,
-    fill = d3.scale.category20();
+function visualizeGraph(name) {
 
-var vis = d3.select("#chart")
-    .append("svg:svg")
-    .attr("width", w)
-    .attr("height", h);
+    var w = 800,
+        h = 600,
+        fill = d3.scale.category20();
 
-d3.json("visualize.json", function(error, json) {
-    if (error) throw error;
+    var vis = d3.select("#chart")
+        .append("svg:svg")
+        .attr("width", w)
+        .attr("height", h);
 
-    var force = d3.layout.force()
-        .nodes(json.nodes)
-        .links(json.links)
-        .size([w, h])
-        .charge(-30)
-        .gravity(0.1)
-        .start();
+    d3.json(name, function (error, json) {
+        if (error) throw error;
 
-    var link = vis.selectAll("line.link")
-        .data(json.links)
-        .enter().append("svg:line")
-        .attr("class", "link")
-        .style("stroke-width", function(d) { return Math.sqrt(d.value); })
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+        var force = d3.layout.force()
+            .nodes(json.nodes)
+            .links(json.links)
+            .size([w, h])
+            .charge(-30)
+            .gravity(0.1)
+            .start();
 
-    var node = vis.selectAll("circle.node")
-        .data(json.nodes)
-        .enter().append("svg:circle")
-        .attr("class", "node")
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; })
-        .attr("r", 5)
-        .style("fill", function(d) { return fill(d.group); })
-        .call(force.drag);
+        var link = vis.selectAll("line.link")
+            .data(json.links)
+            .enter().append("svg:line")
+            .attr("class", "link")
+            .style("stroke-width", function (d) {
+                return Math.sqrt(d.value);
+            })
+            .attr("x1", function (d) {
+                return d.source.x;
+            })
+            .attr("y1", function (d) {
+                return d.source.y;
+            })
+            .attr("x2", function (d) {
+                return d.target.x;
+            })
+            .attr("y2", function (d) {
+                return d.target.y;
+            });
 
-    node.append("svg:title")
-        .text(function(d) { return d.name; });
+        var node = vis.selectAll("circle.node")
+            .data(json.nodes)
+            .enter().append("svg:circle")
+            .attr("class", "node")
+            .attr("cx", function (d) {
+                return d.x;
+            })
+            .attr("cy", function (d) {
+                return d.y;
+            })
+            .attr("r", 5)
+            .style("fill", function (d) {
+                return fill(d.group);
+            })
+            .call(force.drag);
 
-    vis.style("opacity", 1e-5)
-        .transition()
-        .duration(1000)
-        .style("opacity", 1);
+        node.append("svg:title")
+            .text(function (d) {
+                return d.name;
+            });
 
-    force.on("tick", function() {
-        link.attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
+        vis.style("opacity", 1e-5)
+            .transition()
+            .duration(1000)
+            .style("opacity", 1);
 
-        node.attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
+        force.on("tick", function () {
+            link.attr("x1", function (d) {
+                return d.source.x;
+            })
+                .attr("y1", function (d) {
+                    return d.source.y;
+                })
+                .attr("x2", function (d) {
+                    return d.target.x;
+                })
+                .attr("y2", function (d) {
+                    return d.target.y;
+                });
+
+            node.attr("cx", function (d) {
+                return d.x;
+            })
+                .attr("cy", function (d) {
+                    return d.y;
+                });
+        });
     });
-});
+
+}
